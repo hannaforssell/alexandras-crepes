cb633180b0e813638c76bcc4a23531511e2bb66a
Anton Janzon <61318081+antonjanzon95@users.noreply.github.com>
2023-02-24 10:49:47 +0100

    Styled slideshow

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,37 +1,34 @@
-import '../scss/style.scss'
+import "../scss/style.scss";
 
-let sliderIndex = 1;
+let sliderIndex = 2;
 
-const rightSlide = document.querySelector('.right') as HTMLButtonElement;
-const leftSlide = document.querySelector('.left') as HTMLButtonElement;
+const rightSlide = document.querySelector(".right") as HTMLButtonElement;
+const leftSlide = document.querySelector(".left") as HTMLButtonElement;
 
-rightSlide.addEventListener('click', nextSlide);
-leftSlide.addEventListener('click', nextSlide);
+rightSlide.addEventListener("click", nextSlide);
+leftSlide.addEventListener("click", nextSlide);
 
-function showslides (index: number){
-    const rightImg = document.querySelector('#images1') as HTMLImageElement
-    const leftImg = document.querySelector('#images2') as HTMLImageElement;
+function showslides(index: number) {
+  const rightImg = document.querySelector("#images1") as HTMLImageElement;
+  const leftImg = document.querySelector("#images2") as HTMLImageElement;
 
-    if (index === 1){
-        rightImg.classList.remove('hidden');
-        leftImg.classList.add('hidden');
-    }
+  if (index === 1) {
+    leftImg.classList.remove("hidden");
+    rightImg.classList.add("hidden");
+  }
 
-    if (index === 2){
-        rightImg.classList.add('hidden');
-        leftImg.classList.remove('hidden');
-    }
+  if (index === 2) {
+    leftImg.classList.add("hidden");
+    rightImg.classList.remove("hidden");
+  }
 }
 
-function nextSlide(){
-    if(sliderIndex === 1){
-        sliderIndex = 2;
-    }else if(sliderIndex === 2){
-        sliderIndex = 1;
-    }
+function nextSlide() {
+  if (sliderIndex === 1) {
+    sliderIndex = 2;
+  } else if (sliderIndex === 2) {
+    sliderIndex = 1;
+  }
 
-    showslides(sliderIndex);
-}  
-
-
-
+  showslides(sliderIndex);
+}
