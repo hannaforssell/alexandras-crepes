09435331dac80b99ac165a50a3602026be35a86e
Mattias heurlin <mattias.heurlin@hotmail.com>
2023-02-24 09:58:24 +0100

    SliderFunction

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,37 @@
 import '../scss/style.scss'
+
+let sliderIndex = 1;
+
+const rightSlide = document.querySelector('.right') as HTMLButtonElement;
+const leftSlide = document.querySelector('.left') as HTMLButtonElement;
+
+rightSlide.addEventListener('click', nextSlide);
+leftSlide.addEventListener('click', nextSlide);
+
+function showslides (index: number){
+    const rightImg = document.querySelector('#images1') as HTMLImageElement
+    const leftImg = document.querySelector('#images2') as HTMLImageElement;
+
+    if (index === 1){
+        rightImg.classList.remove('hidden');
+        leftImg.classList.add('hidden');
+    }
+
+    if (index === 2){
+        rightImg.classList.add('hidden');
+        leftImg.classList.remove('hidden');
+    }
+}
+
+function nextSlide(){
+    if(sliderIndex === 1){
+        sliderIndex = 2;
+    }else if(sliderIndex === 2){
+        sliderIndex = 1;
+    }
+
+    showslides(sliderIndex);
+}  
+
+
+
